4b7c984ea0b43abc0818d69022fe2ba4da575fa7
Renato Gomes de Sousa <renattoengineer@gmail.com>
2023-02-02 10:59:03 -0300

    Exercício 003
    Faça um algoritmo utilizando o comando while que mostra uma contagem regressiva na tela, iniciando em 10 e terminando em 0. Mostrar uma mensagem “FIM!” após a contagem.

---
+++ ExerciciosResolvidos/ex003.c
@@ -0,0 +1,24 @@
+/*
+ * ex003.c
+ *
+ *  Criado em: 2 de fev de 2023
+ *      Autor: Renato Gomes de Sousa
+ *
+ *      https://github.com/RenatoEngineer
+ */
+
+#include <stdio.h>
+
+int main(){
+
+	int contador = 11;
+
+	while (contador > 0){
+		contador = contador - 1;
+		printf("%d\n", contador);
+	}
+
+	printf("\nFIM");
+
+	return 0;
+}
