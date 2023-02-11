568bc31e3deed4ba988f880cc7fdd4b3ad6c129d
Renato Gomes de Sousa <renattoengineer@gmail.com>
2023-02-10 22:21:32 -0300

    Exercício 013
    Faça um programa que leia um número inteiro positivo par N e imprima todos os números pares de 0 até N em ordem crescente.

---
+++ ExerciciosResolvidos/ex013.c
@@ -0,0 +1,30 @@
+/*
+ * ex013.c
+ *
+ *  Criado em: 10 de fev de 2023
+ *      Autor: Renato Gomes de Sousa
+ *
+ *      https://github.com/RenatoEngineer
+ */
+
+#include <stdio.h>
+
+int main(){
+
+	int numero;
+	int contador;
+
+	printf("Digite um Numero Inteiro Positivo: ");
+	fflush(stdout);
+	scanf("%d", &numero);
+
+	if(numero > 0 && numero %2 == 0){
+		for(contador = 0; contador <= numero; contador = contador + 2){
+			printf("%d ", contador);
+		}
+	}else{
+		printf("\nNumero Invalido!!!");
+	}
+
+	return 0;
+}
